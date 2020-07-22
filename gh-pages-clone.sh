c25ef446003957aaf361710b2aca71d47a2cdfb9
niravassar <assarn@objectcomputing.com>
2020-07-22 16:28:09 -0500

    make two different scripts so we can reuse

--- gh-pages-clone.sh
+++ gh-pages-clone.sh
@@ -3,11 +3,4 @@ git config --global user.email "$GIT_EMAIL"
 git config --global credential.helper "store --file=~/.git-credentials"
 echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 git clone https://${GH_TOKEN}@github.com/${GIT_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
-cd gh-pages
-mkdir -p snapshot
-cp -r ../build/docs/. ./snapshot/
-git add snapshot/*
-git commit -a -m "Updating docs"
-# DONT RUN -- git push origin HEAD
-cd ..
-rm -rf gh-pages
+
