d7fa0fbc01772e76f1345f6ddb6b63f6dd607ae4
niravassar <assarn@objectcomputing.com>
2020-07-23 15:20:35 -0500

    get the tag name for latest folder

--- gh-pages-latest-docs.sh
+++ gh-pages-latest-docs.sh
@@ -6,15 +6,13 @@ git add latest/*
 
 version=$GITHUB_REF # eg: refs/tags/v8.1.1
 version=${version:11} # 3.0.1
-#majorVersion=${version:0:4} # 3.0.
-#majorVersion="${majorVersion}x" # 3.0.x
-#mkdir -p "$version"
-#cp -r ../build/docs/. "./$version/"
-#git add "$version/*"
-#git rm -rf "$majorVersion"
-#mkdir -p "$majorVersion"
-#cp -r ../build/docs/. "./$majorVersion/"
-#git add "$majorVersion/*"
-#ls -al
-
-echo "************* $version *******"
+majorVersion=${version:0:4} # 3.0.
+majorVersion="${majorVersion}x" # 3.0.x
+mkdir -p "$version"
+cp -r ../build/docs/. "./$version/"
+git add "$version/*"
+git rm -rf "$majorVersion"
+mkdir -p "$majorVersion"
+cp -r ../build/docs/. "./$majorVersion/"
+git add "$majorVersion/*"
+ls -al
