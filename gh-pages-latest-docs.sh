6bbebfd1e195f7d25bcd126ed7297d971609df22
niravassar <assarn@objectcomputing.com>
2020-07-23 15:17:06 -0500

    parse the variable

--- gh-pages-latest-docs.sh
+++ gh-pages-latest-docs.sh
@@ -3,17 +3,18 @@ git rm -rf latest/
 mkdir -p latest
 cp -r ../build/docs/. ./latest/
 git add latest/*
-version="v8.1.1" # eg: v3.0.1
-version=${version:1} # 3.0.1
-majorVersion=${version:0:4} # 3.0.
-majorVersion="${majorVersion}x" # 3.0.x
-mkdir -p "$version"
-cp -r ../build/docs/. "./$version/"
-git add "$version/*"
-git rm -rf "$majorVersion"
-mkdir -p "$majorVersion"
-cp -r ../build/docs/. "./$majorVersion/"
-git add "$majorVersion/*"
-ls -al
 
-echo "************* $GITHUB_REF *******"
+version=$GITHUB_REF # eg: refs/tags/v8.1.1
+version=${version:11} # 3.0.1
+#majorVersion=${version:0:4} # 3.0.
+#majorVersion="${majorVersion}x" # 3.0.x
+#mkdir -p "$version"
+#cp -r ../build/docs/. "./$version/"
+#git add "$version/*"
+#git rm -rf "$majorVersion"
+#mkdir -p "$majorVersion"
+#cp -r ../build/docs/. "./$majorVersion/"
+#git add "$majorVersion/*"
+#ls -al
+
+echo "************* $version *******"
